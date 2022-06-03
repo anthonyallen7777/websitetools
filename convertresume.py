d6cb6f2ff1448d382cf1965cd594c223ded26a94
anthonyallengithub <41088309+anthonyallengithub@users.noreply.github.com>
2022-06-03 11:59:21 -0500

    working converter

--- convertresume.py
+++ convertresume.py
@@ -4,14 +4,18 @@
 #install poppler
 
 import sys
+import os
 from pdf2image import convert_from_path
+#current directory
+directory = os.getcwd()
+pPath = directory+R"\poppler-22.04.0\Library\bin"
 
 def convert():
 	for arg in sys.argv[1:]:
 		t = 0
-		pages = convert_from_path(arg, 500)
+		pages = convert_from_path(arg, 500,poppler_path=pPath)
 		for page in pages:
-		#	page.save(t+'.png','PNG')
+			page.save('resume'+str(t)+'.png','PNG')
 			print(t,'\n')
 			t=t+1
 
